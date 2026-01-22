1e3c0e895b0547b31f8124f04cc550bff577221b
Giovannypl <gestionhumanaylogistica@gmail.com>
2026-01-21 20:12:50 -0500

    Actualizar server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware para parsear JSON
 app.use(express.json());
