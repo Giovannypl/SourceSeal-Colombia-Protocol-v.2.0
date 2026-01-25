d00ccb2adf601263768545c5eca4b44d87d93612
Giovannypl <gestionhumanaylogistica@gmail.com>
2026-01-24 22:17:17 -0500

    Actualizar server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 if (process.env.REPL_ID) {
   const express = require('express');
   const app = express();
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 5000;
   app.use(express.static('public'));
   app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
   app.get('/api/status', (req, res) => res.json({
